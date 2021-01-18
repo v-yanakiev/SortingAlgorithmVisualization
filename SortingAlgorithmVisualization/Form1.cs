ec19834ac0676ae8e9e916307e0f5d13451b3e1b
vaskoleon <vaskoleon@gmail.com>
2021-01-18 21:23:55 +0200

    init

---
+++ SortingAlgorithmVisualization/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SortingAlgorithmVisualization
+{
+    public partial class Form1 : Form
+    {
+        private Random random;
+        const int sizeOfData = 10;
+        public int[] arrayToSort;
+        public Form1()
+        {
+            InitializeComponent();
+            this.random = new Random();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            for (int i = 0; i < sizeOfData; i++)
+            {
+                stringBuilder.Append(random.Next(1,1000));
+                if (i != sizeOfData - 1)
+                    stringBuilder.Append(", ");
+            }
+            textBox1.Text = stringBuilder.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == null || textBox1.Text.Trim() == "")
+            {
+                return;
+            }
+            try
+            {
+                arrayToSort = textBox1.Text.Split(new string[] { ", " }, StringSplitOptions.RemoveEmptyEntries).
+                    Select(a => int.Parse(a)).ToArray();
+            }
+            catch
+            {
+                return;
+            }
+            Form2 form2 = new Form2(this);
+            this.Hide();
+            form2.Show();
+            
+        }
+    }
+}
---
+++ SortingAlgorithmVisualization/Form2.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SortingAlgorithmVisualization
+{
+    public partial class Form2 : Form
+    {
+        private Form1 callingForm;
+        private int[] arrayToSort;
+        List<Label> labels;
+        int i, j,indexOfCurrentMinimum;
+        private bool delay;
+
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        public Form2(Form1 form1)
+            : this()
+        {
+            this.callingForm = form1;
+            this.arrayToSort = form1.arrayToSort;
+            this.labels = new List<Label>();
+            int locationX = 30;
+            int locationY = 0;
+            foreach (var number in callingForm.arrayToSort)
+            {
+                Label label = new Label();
+
+                label.Text = string.Format("{0,3:###}",number);
+                label.Location = new Point(locationX, locationY);
+                label.AutoSize = true;
+                label.Font = new Font("Courier", 18);
+                label.ForeColor = Color.Black;
+                label.Padding = new Padding(10);
+                label.BorderStyle = BorderStyle.FixedSingle;
+                locationX += 70;
+                this.Controls.Add(label);
+                this.labels.Add(label);
+            }
+            i=0;
+            j = i + 1;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (delay)
+            {
+                delay = false;
+            } 
+            else
+            {
+                if (j >= arrayToSort.Length && i >= (arrayToSort.Length - 1))
+                {
+                    labels[labels.Count - 1].BackColor = Color.Green;
+                    return;
+                }
+                if (labels[j - 1].BackColor != Color.LightGreen && labels[j - 1].BackColor != Color.OrangeRed)
+                {
+                    labels[j - 1].BackColor = Color.Transparent;
+                }
+                if (j == i + 1)
+                {
+
+                    indexOfCurrentMinimum = i;
+                    labels[i].BackColor = Color.OrangeRed;
+                }
+                labels[j].BackColor = Color.LightBlue;
+                if (arrayToSort[j] < arrayToSort[indexOfCurrentMinimum])
+                {
+                    if (labels[indexOfCurrentMinimum].BackColor == Color.LightGreen)
+                    {
+                        labels[indexOfCurrentMinimum].BackColor = Color.Transparent;
+                    }
+                    indexOfCurrentMinimum = j;
+                    labels[indexOfCurrentMinimum].BackColor = Color.LightGreen;
+                }
+                j++;
+                if (j == arrayToSort.Length)
+                {
+                    delay = true;
+                    return;
+                }
+            }
+
+            if (j == arrayToSort.Length)
+            {
+                if (i != arrayToSort.Length - 1)
+                {
+                    labels[indexOfCurrentMinimum].BackColor = Color.Transparent;
+                }
+                labels[labels.Count - 1].BackColor = Color.Transparent;
+                int temp = arrayToSort[indexOfCurrentMinimum];
+                arrayToSort[indexOfCurrentMinimum] = arrayToSort[i];
+                labels[indexOfCurrentMinimum].Text = string.Format("{0,3:###}", arrayToSort[i]);
+                arrayToSort[i] = temp;
+                labels[i].Text = string.Format("{0,3:###}", temp);
+                labels[i].BackColor = Color.Green;
+                i++;
+                j = i + 1;
+            }
+        }
+    }
+}
